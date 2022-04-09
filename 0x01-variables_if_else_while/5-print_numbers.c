e282b336ceb8168064a80db311ca63759a7191cf
Stemoo <momohkeshi@yahoo.com>
2022-04-09 08:19:46 -0700

    push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prin numbers from 0 to 10
+ *
+ * Returns: Always (Success)
+ *
+ */
+
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb <=9; numb++)
+		printf(numb);
+
+	printf('\n');
+
+	return (0);
+}
