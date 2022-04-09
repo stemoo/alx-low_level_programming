3ad363c26d4d1058928f4efcf0e6cc73ce09352a
Stemoo <momohkeshi@yahoo.com>
2022-04-09 10:01:25 -0700

    push

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - To print all possible collections of two digits without repeating numbers
+ * 
+ * Return: Always(Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <='9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
