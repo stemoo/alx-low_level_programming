714f4be9f36c243b29d9c5cb554c7b1f01229886
Stemoo <momohkeshi@yahoo.com>
2022-04-09 07:46:48 -0700

    push

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print a - z in small letters
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(alph);
+
+	putchar('\n');
+	return (0);
+}
