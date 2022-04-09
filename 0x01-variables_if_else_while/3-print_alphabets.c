83f227ba118d7b2bbec041875ba91d166fa81e28
Stemoo <momohkeshi@yahoo.com>
2022-04-09 07:55:14 -0700

    push

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Inout alphabets in lower and upper case
+ *
+ * Return: Always (Sucess_
+ */
+int main(void)
+{
+	char alph, ALPH;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+
+	for (ALPH = 'A', ALPH <= 'Z'; ALPH++)
+	{
+		putchar(ALPH);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
