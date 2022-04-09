be58d08b82ad2fdf0891e7fac897b60bd49ca971
Stemoo <momohkeshi@yahoo.com>
2022-04-09 09:08:34 -0700

    push

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers from zeroo to nine separated by a comma
+ *
+ * Return: Always(Success)
+ */
+
+int main(void)
+{
+	int numb;
+
+	for (numb = '0'; numb <= '9'; numb++)
+	{
+		putchar(numb);
+		if (numb != 9)
+		{
+			putchar(', ');
+		}
+	}
+
+
