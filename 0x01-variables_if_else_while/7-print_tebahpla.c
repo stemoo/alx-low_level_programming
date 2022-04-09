8de8b3e6335e2d4acc3aefed1370a47824f27e0a
Stemoo <momohkeshi@yahoo.com>
2022-04-09 08:42:52 -0700

    push

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print z - a
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+		putchar(alph)
+	putchar('\n')
+
+	return (0)
+}
