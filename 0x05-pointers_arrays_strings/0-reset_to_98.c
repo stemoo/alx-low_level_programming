bf94e0573ebe94ddabcd85d953b17cd3fe5e9f19
Stemoo <momohkeshi@yahoo.com>
2022-04-20 16:47:30 -0700

    push

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* reset_to_98 - updates the value it points
-*@n: pointer of the direcction at the variable n
-*Return: 0 (success)
-*/
+ * reset_to_98 - updates the value it points
+ * @n: pointer of the direcction at the variable n
+ * Return: 0 (success)
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
