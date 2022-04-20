56159036fc2cb790aea350966df47fa7d4cd096e
Stemoo <momohkeshi@yahoo.com>
2022-04-20 16:41:26 -0700

    push

---
+++ 0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print a statement
+ *
+ * Return: Always(Success)
+ */
+
+int manin(void)
+{
+	/**
+	 * main function is to print *putchar
+	 */
+
+	printf('_putchar);
+	
+	return (0);
+}
